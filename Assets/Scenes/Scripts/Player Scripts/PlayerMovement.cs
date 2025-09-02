50e093f00841e66fd01bac3fead1766d09a0f02c
Andrea Hila <94169535+andhil19@users.noreply.github.com>
2025-09-02 16:10:49 +0200

    Jumping Platforms
    added box colliders some platforms

--- Assets/Scenes/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/Player Scripts/PlayerMovement.cs
@@ -8,7 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
     private Rigidbody2D rb;
-   // private bool isGrounded = true;
+
+    private bool isGrounded = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +23,17 @@ public class PlayerMovement : MonoBehaviour
         HandleMovement();
 
     }
+
     private void HandleMovement()
     {
         Vector3 movement = Vector3.zero;
 
-        if (Input.GetKeyDown(KeyCode.W) ) //&& isGrounded
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            //isGrounded = false; 
+            isGrounded = false;
         }
-       // if (Input.GetKey(KeyCode.S)) movement += Vector3.down;
+        //if (Input.GetKey(KeyCode.S)) movement += Vector3.down;
         if (Input.GetKey(KeyCode.A)) movement += Vector3.left;
         if (Input.GetKey(KeyCode.D)) movement += Vector3.right;
         if (movement != Vector3.zero) movement.Normalize();
@@ -38,14 +41,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 newPosition = transform.position + movement * (speed * Time.deltaTime);
         transform.position = newPosition;
     }
-   /* private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+
         // Simple ground check (tag your ground as "Ground")
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
     }
-    */
+
+
 
 }
