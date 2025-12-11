b5987af67e878e3a940e8a6b1dcc13beddcd4f87
glolaz19 <96109188+glolaz19@users.noreply.github.com>
2025-12-11 01:11:53 +0100

    animatiom
    animation

--- Assets/Prefabs/Scripts/Menus/StartMenu.cs
+++ Assets/Prefabs/Scripts/Menus/StartMenu.cs
@@ -6,29 +6,24 @@ public class StartMenu : MonoBehaviour
     
     public void StartGame()
     {
-        // Load your game scene
         SceneManager.LoadScene("TheBunkerScene");
         Debug.Log("StartGame");
     }
-
     public void OpenLevels()
     {
-        // Load your level selection scene
         SceneManager.LoadScene("LevelSelect");
     }
 
     public void OpenSettings()
     {
-        // Load your settings scene
         SceneManager.LoadScene("Settings");
     }
 
     public void QuitGame()
     {
-        // Quit application
         Application.Quit();
 
-        // If running in editor
+      
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
